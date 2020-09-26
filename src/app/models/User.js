93104ddfbc05117ffc65dc6fbac3c67013f8daee
Horacio <oicaroh64@gmail.com>
2020-09-26 07:56:21 -0300

    Implementacao da criptografia da senha com bcryptjs

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,12 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
 
 class User extends Model {
     static init(sequelize) {
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             password_hash: Sequelize.STRING,
             provider: Sequelize.BOOLEAN
         },
@@ -13,6 +15,14 @@ class User extends Model {
                 
             }
         )
+
+        this.addHook('beforeSave', async (user)=>{
+            if(user.password){
+                user.password_hash = await bcrypt.hash(user.password,8)
+            }
+        })
+
+        return this
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,8 @@
 import {Router} from 'express'
 import User from './app/models/User'
+import UserController from './app/controllers/UserController'
 const routes = new Router()
 
-routes.get('/', async (req,res)=>{
-    const user = await User.create({
-        name: 'Dede',
-        email: 'dede@gmail.com',
-        password_hash: '59844879'
-    })
-
-    const user2 = await User.create({
-        name: 'Mussun',
-        email: 'mu@gmail.com',
-        password_hash: '16844879'
-    })
-    return res.json(user2)
-})
+routes.post('/users', UserController.store)
 
 export default routes
