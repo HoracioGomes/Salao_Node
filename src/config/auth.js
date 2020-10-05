07c65d99ae8967109c005c351f4f88c630ccacd8
Horacio <oicaroh64@gmail.com>
2020-10-05 07:21:54 -0300

    Implementacao inicial do Token

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'textoAleatorio',
+    expiresIn: '7d'
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,18 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import User from './app/models/User'
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import authMiddleware from './app/middlewares/auth'
 const routes = new Router()
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
+
+// Usar middleware em uma unica rota
+// routes.put('/users',authMiddleware, UserController.update)
+//Usar middlewares para todas as rotas subsequentes
+routes.use(authMiddleware)
+routes.put('/users', UserController.update)
+
 
 export default routes
