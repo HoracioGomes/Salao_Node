5c1c0e8eba013bc54ce981b8cfba89a1a75b4476
Horacio <oicaroh64@gmail.com>
2020-10-11 15:28:03 -0300

    Adicao EsLint, prettier e editorConfig + passgem de crlf para lf

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,22 +1,23 @@
-import jwt from 'jsonwebtoken'
-import authConfig from '../../config/auth'
-import { promisify } from 'util'
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
 export default async (req, res, next) => {
-    const authHeader = req.headers.authorization
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-        return res.status(401).json({ error: 'Token não encontrado' })
-    }
-    //split dividindo Bearer do codigo do token
-    //virgula dentro dos colchetes esta ignorando primeiro parametro(Bearer)
-    const [, token] = authHeader.split(' ')
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não encontrado' });
+  }
+  // split dividindo Bearer do codigo do token
+  // virgula dentro dos colchetes esta ignorando primeiro parametro(Bearer)
+  const [, token] = authHeader.split(' ');
 
-    try {
-        const decoded = await promisify(jwt.verify)(token,authConfig.secret)
-        console.log(decoded);
-        req.userId = decoded.id
-        return next()
-    } catch (error) {
-        return res.status(401).json({ error: 'Token Inválido!' })
-    }
-}
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    console.log(decoded);
+    req.userId = decoded.id;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token Inválido!' });
+  }
+};
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-    secret: 'textoAleatorio',
-    expiresIn: '7d'
-}
+  secret: 'textoAleatorio',
+  expiresIn: '7d',
+};
